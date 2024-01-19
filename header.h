a2f7922f90c04aae3115d63254861fda1930c4b7
Nathan Callon <ncallon@pdx.edu>
2024-01-19 15:40:10 -0800

    Draft header.h prog#1

---
+++ header.h
@@ -0,0 +1,154 @@
+/* Nathan Callon, CS302, Karla Fant, 1/19/2024
+In this program I'll be constructing races using 2 different parent classes and 3 different
+child classes for each base class. The parent classes will not be instantiated in the main program
+but the children will, and this will help reduce redundancy in the code. The races will be
+separated into Snowmobilers, Snowboarders and Skiiers.*/
+
+
+#ifndef _SNOW_
+#define _SNOW_
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <stdlib.h>
+
+using namespace std;
+
+/* The Person class is the parent class for the
+snowboarders, snowmobilers and skiiers, and it includes 
+functions that all of them will share and some of which may be overwritten. */
+class Person { // Parent class for all types of participants
+  public:
+    Person(); // Base constructor
+    Person(const char *aName); // Constructor with arguments
+    Person(const Person& aPerson); // Deep copy constructor
+    Person operator=(const Person& aPerson); // Copy aPerson into itself with new allocated memory
+    ~Person(); // Destructor
+
+    const char *getName() const; // Return name of participant
+    void setFinished(); // Declare that the racer has finished the race
+    const bool getFinished() const; // Return whether or not the racer has finished the race
+  protected:
+    int secretNumber; // Secret number to use RNG to determine how the racer acts
+    char *name; // Used to allocate memory for the name of the racer
+    bool isFinished; // Keeps track of whether the racer has finished
+  private:
+};
+
+class Snowmobiler: public Person {
+  public:
+    Snowmobiler();
+    Snowmobiler(const char *aName);
+    Snowmobiler(const Snowmobiler& aSnowmobiler);
+    Snowmobiler operator=(const Snowmobiler& aSnowmobiler);
+    ~Snowmobiler();
+
+    void changeSpeed(const int secretNumber); // Changes the speed of the snowmobile depending on the number passed in
+  private:
+    int gasLevel; // Keeps track of gas level of the snowmobile
+    int distanceTraveled; // Keeps track of distance traveled of the snowmobile
+};
+
+class Snowboarder: public Person {
+  public: 
+    Snowboarder();
+    Snowboarder(const char *aName);
+    Snowboarder(const Snowboarder& aSnowboarder);
+    Snowboarder operator=(const Snowboarder& aSnowboarder);
+    ~Snowboarder();
+
+    Snowboarder *getNext(); // Retrieves the next snowboarder as they are attached through a LLL
+    void *setNext(Snowboarder *aSnowboarder); // Sets the next snowboarder node
+    int performTrick(int aNumber); // Performs a trick returning a score based on the number passed in
+  protected:
+
+  private:
+    Snowboarder *next; // Next node to attach nodes throughout LLL
+};
+
+class Skiier: public Person {
+  public: 
+    Skiier();
+    Skiier(const char *aName);
+    Skiier(const Skiier& aSkiier);
+    Skiier operator=(const Skiier& aSkiier);
+    ~Skiier();
+
+    Skiier *getNext(); // Returns next Skiier from CLL
+    void SetNext(Skiier *aSkiier); // Sets next Skiier node attached to this node
+    Skiier *getPrev(); // Returns previous Skiier from CLL
+    void setPrev(Skiier *aSkiier); // Sets previous Skiier node attached to this node
+  private:
+    Skiier *next; // Next node to attach nodes through CLL
+    Skiier *prev; // Previous node to attach nodes through CLL
+};
+
+
+/* Like the parent Person class not being instantiated, the same will
+happen with this parent Race class which has three derived classes
+for each type of race, each of which will be used in the program */
+class Race {
+  public:
+    Race(); // Default constructor will construct a race that has no racers and has not started,
+    // has not ended, and generates a random number to determine how the racers act
+    ~Race();
+
+  protected:
+    int secretNumber; // This secret number will determine how contestants interact in the event
+    // When combined with their own secret numbers
+    bool hasStarted; // This determines whether the race has begun or not
+    bool isFinished; // This determines whether the race is finished or not
+  private:
+};
+
+
+class MobileRace: public Race {
+  public:
+    MobileRace(); // Default constructor will call parent constructor and initialize empty vector of racers
+    MobileRace(const vector<Snowmobiler> snowmobilers); // This constructor is same as default but will pre-build the racers
+    ~MobileRace();
+    void addRacer(const Snowmobiler& aSnowmobiler); // This is for adding snowmobilers to the race
+    // After MobileRace has already been initialized
+
+    bool startRace();
+    bool endRace();
+    Person determineWinner(); // Determines the winner based on the furthest distance traveled
+  protected:
+    vector<Snowmobiler> racers; // The snowmobile race will have the racers stored in a vector
+
+  private:
+};
+
+class BoarderRace: public Race {
+  public:
+    BoarderRace(); // Default constructor will call parent constructor and have an empty head pointer of LLL of racers
+    BoarderRace(const Snowboarder * snowboarderHead); // This constructor is same as default but will pre-build the racers
+    ~BoarderRace();
+    void addRacer(const Snowboarder& aSnowboarder); // Adds a racer to the tail end 
+    bool startRace();
+    bool endRace();
+    Person determineWinner(); // Determines the winner based on the highest score from tricks
+  protected:
+    Snowboarder * head; // Head pointer to linear linked list of snowboarders
+
+  private:
+};
+
+
+class SkiierRace: public Race {
+  public:
+    SkiierRace(); // Default constructor will call parent constructor and have an empty head pointer of CLL of racers
+    SkiierRace(const Skiier * skiierHead); // This constructor is same as default but will pre-build the racers
+    ~SkiierRace();
+
+    bool startRace();
+    bool endRace();
+    Person determineWinner(); // Determines a winner based on who finished first
+  protected:
+    Skiier * head; // Head pointer to circular linked list of skiiers
+
+  private:
+};
+
+#endif // _SNOW_
