e924abf44b6c115cb304936070db9059464cf4b2
Nathan Callon <ncallon@pdx.edu>
2024-01-19 15:43:17 -0800

    Draft header.h Program#1

---
+++ Callon-Nathan-Prog1.cpp
@@ -0,0 +1,30 @@
+#include "header.h"
+
+int main() {
+  
+  MobileRace aRace;
+
+  Skiier aSkiier("test");
+
+  Skiier aSkiier2(aSkiier);
+
+  Skiier aSkiier3 = aSkiier2;
+
+  cout << aSkiier2.getName() << "\n";
+
+  cout << aSkiier3.getName() << "\n";
+
+//  int test = rand() % 100 + 1;
+
+  char *name = new char[5];
+  strcpy(name, "test");
+
+  Person aPerson(name);
+
+  Person person2;
+
+  delete [] name;
+
+  return 0;
+}
+
---
+++ implementation.cpp
@@ -0,0 +1,152 @@
+#include "header.h"
+
+Person::Person(): name(nullptr) {
+  this->secretNumber = rand() % 1000 + 1;
+  this->isFinished = false;
+}
+
+Person::Person(const char *aName) { // Constructor with arguments
+  this->name = new char[strlen(aName)+1]; //Allocate separate memory for Person's name
+  strcpy(this->name, aName); //Copy characters from other name to Person's name
+  this->isFinished = false;
+}
+
+Person::Person(const Person& aPerson) { // Deep copy constructor
+  if(aPerson.name == nullptr) this->name = nullptr; // If other person does not have a name set name to null
+  else 
+  {
+    this->name = new char[strlen(aPerson.name)+1];
+    strcpy(this->name, aPerson.name); // Otherwise set it to their name
+  }
+  this->isFinished = aPerson.isFinished;
+}
+
+Person Person::operator=(const Person& aPerson) { // Equal overload
+  if(aPerson.name == nullptr) this->name = nullptr; // If other person does not have a name set name to null
+  else 
+  {
+    this->name = new char[strlen(aPerson.name)+1];
+    strcpy(this->name, aPerson.name); // Otherwise set it to their name
+  }
+  this->isFinished = aPerson.isFinished;
+  return *this;
+}
+
+Person::~Person() { // Destructor
+  if(name) delete [] name; // If memory allocated for name delete name
+  name = nullptr; // Remove pointer to memory
+}
+
+const char * Person::getName() const { // Getter for Person's name
+  if(name == nullptr) return ""; // Doesn't seem necessary but if you std::cout a nullptr, the output stream will be disabled.
+  else return name; // If name set return their name
+}
+
+void Person::setFinished() {
+  isFinished = true;
+}
+
+const bool Person::getFinished() const {
+  return isFinished;
+}
+
+Race::Race() {
+  secretNumber = rand() % 1000 + 1; // Generates random number between 1 and 1000
+  hasStarted = false; // Sets race to not started
+  isFinished = false; // Sets race to not finished
+}
+
+Race::~Race() {
+}
+
+Snowmobiler::Snowmobiler() {
+  Person();
+  gasLevel = 100;
+  distanceTraveled = 0;
+}
+
+Snowmobiler::Snowmobiler(const Snowmobiler& aSnowmobiler): Person(aSnowmobiler) {
+}
+
+Snowmobiler Snowmobiler::operator=(const Snowmobiler& aSnowmobiler) {
+  if(aSnowmobiler.name == nullptr) this->name = nullptr; // If other person does not have a name set name to null
+  else 
+  {
+    this->name = new char[strlen(aSnowmobiler.name)+1];
+    strcpy(this->name, aSnowmobiler.name); // Otherwise set it to their name
+  }
+  this->gasLevel = aSnowmobiler.gasLevel;
+  this->secretNumber = aSnowmobiler.secretNumber;
+  return *this;
+}
+
+Snowmobiler::~Snowmobiler() {
+}
+
+Snowboarder::Snowboarder(): Person() {
+  this->next = nullptr;
+}
+
+Snowboarder::Snowboarder(const char* aName): Person(aName) {
+  this->next = nullptr;
+}
+
+Snowboarder::Snowboarder(const Snowboarder& aSnowboarder): Person(aSnowboarder) {
+  this->next= nullptr;
+}
+
+Snowboarder::~Snowboarder() {
+
+}
+
+Snowboarder * Snowboarder::getNext() {
+  return this->next;
+}
+
+Skiier::Skiier(): Person() {
+  this->next = nullptr;
+  this->prev = nullptr;
+}
+
+Skiier::Skiier(const char* aName): Person(aName) {
+  this->next = nullptr;
+  this->prev = nullptr;
+}
+
+Skiier::Skiier(const Skiier& aSkiier): Person(aSkiier) {
+  this->next = nullptr;
+  this->prev = nullptr;
+}
+
+Skiier::~Skiier() {
+
+}
+
+MobileRace::MobileRace(): Race() {
+}
+
+MobileRace::MobileRace(const vector<Snowmobiler> snowmobilers): Race() {
+  for(long unsigned int i = 0; i < snowmobilers.size(); i++)
+    racers.insert(racers.begin(), snowmobilers[i]);
+}
+
+MobileRace::~MobileRace() {
+
+}
+
+BoarderRace::BoarderRace() {
+
+}
+
+BoarderRace::~BoarderRace() {
+  Snowboarder *traverser, *forward = nullptr;
+  traverser = this->head;
+  forward = this->head->getNext();
+  while(traverser != nullptr) {
+    delete traverser;
+    traverser = forward;
+    forward = forward->getNext();
+  }
+}
+
+
