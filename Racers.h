8a5c00a88ecede545bde737c117faf747b975a51
Nathan Callon <ncallon@pdx.edu>
2024-01-31 18:28:41 -0800

    Separating header and implementation files for organization

--- Racers.h
+++ Racers.h
@@ -4,8 +4,8 @@ child classes for each base class. The parent classes will not be instantiated i
 but the children will, and this will help reduce redundancy in the code. The races will be
 separated into Snowmobilers, Snowboarders and Skiiers.*/
 
-#ifndef _SNOW_
-#define _SNOW_
+#ifndef _RACERS_
+#define _RACERS_
 
 #include <iostream>
 #include <cstring>
@@ -110,93 +110,4 @@ private:
   string name;
 };
 
-/* Like the parent Person class not being instantiated, the same will
-happen with this parent Race class which has three derived classes
-for each type of race, each of which will be used in the program */
-class Race
-{
-public:
-  Race(); // Default constructor will construct a race that has no racers and has not started,
-  // has not ended, and generates a random number to determine how the racers act
-  ~Race();
-
-protected:
-  // When combined with their own secret numbers
-  bool hasStarted; // This determines whether the race has begun or not
-  bool isFinished; // This determines whether the race is finished or not
-private:
-};
-
-class MobileRace : public Race
-{
-public:
-  MobileRace();                                       // Default constructor will call parent constructor and initialize empty vector of racers
-  ~MobileRace();
-
-  bool addSnowmobiler(const char * aName); // This is for adding snowmobilers to the race
-  bool remove(const char * aName);
-  bool removeAll();
-  bool display();
-
-  bool startRace();
-  bool endRace();
-  Snowmobiler determineWinner(); // Determines the winner based on the furthest distance traveled
-protected:
-
-private:
-  vector<Snowmobiler> racers; // The snowmobile race will have the racers stored in a vector
-};
-
-class BoarderRace : public Race
-{
-public:
-  BoarderRace(); // Default constructor will call parent constructor and have an empty head pointer of LLL of racers
-  BoarderRace(const BoarderRace& aRace);
-  BoarderRace operator=(const BoarderRace& aRace);
-  ~BoarderRace(); // Dellocates the LLL of racers
- 
-  bool addSnowboarder(const string aName);
-  bool remove(const string aName);
-  bool removeAll();
-  bool display();
-
-  bool startRace();
-  bool endRace();
-  bool displayWinner(); // Determines the winner based on the highest score from tricks
-protected:
-  bool remove(const string aName, Snowboarder *curr, Snowboarder *temp);
-  bool removeAll(Snowboarder *curr);
-  bool display(Snowboarder *curr);
-  Snowboarder * displayWinner(Snowboarder *curr, Snowboarder *winner); // Determines the winner based on the highest score from tricks
-
-private:
-  Snowboarder *head; // Head pointer to linear linked list of snowboarders
-};
-
-class SkiierRace : public Race
-{
-public:
-  SkiierRace();                         // Default constructor will call parent constructor and have an empty head pointer of CLL of racers
-  SkiierRace(const SkiierRace& aRace);
-  SkiierRace operator=(const SkiierRace& aRace);
-  ~SkiierRace();
-
-  bool addSkiier(const string aName);
-  bool remove(const string aName);
-  bool removeAll();
-  bool display();
-
-  bool startRace();
-  bool endRace();
-  bool displayWinner(); // Determines a winner based on who finished first
-protected:
-  bool remove(const string aName, Skiier *curr, Skiier *temp);
-  bool removeAll(Skiier *head);
-  bool display(Skiier *curr);
-  Skiier * displayWinner(Skiier *curr, Skiier *winner); 
-
-private:
-  Skiier *head; // Head pointer to circular linked list of skiiers
-};
-
-#endif // _SNOW_
+#endif // _RACERS_
